03e64a2d0f8a90ee12dcc930f29ccec89c1b6354
Meg Desko <mdesko@gmail.com>
2012-08-29 22:05:07 -0700

    Adding files to see if this works

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+# putting # in front of a line comments it out
+#print "Hello Again"
+print "I like typing this!"
+print "This is fun"
+print 'yay!  Printing!'
+# single quotes inside of double quoted string
+print "I'd much rather you 'not'."
+# double quotes inside of single quoted string
+print 'I "said" do not touch this!'
+print "Bill is cute!"
---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment is hidden from the interpreter
+#by the octothorpe
+# anything after the # is ignored
+
+print "I could have code like this." #and the comment will be ignored
+#you can use a comment to "disable" code
+#print "This won't run"
+ 
+print "This will run"
